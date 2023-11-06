66ed8b443b61d08974f2313aaf5442784f183ba4
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-11-06 19:49:28 -0300

    aula 19

--- src/App.js
+++ src/App.js
@@ -12,34 +12,38 @@ class App extends Component {
   }
 
   loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
-  
-    const [posts] = await Promise.all([postsResponse]);
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
 
     const postsJson = await posts.json();
+    const photosJson = await photos.json();
 
-    this.setState({ posts: postsJson })
-  
-  }
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
 
-  componentDidUpdate() {
-  }
-
-  componentWillUnmount() {
-  }
-
+    this.setState({ posts: postsAndPhotos });
+  };
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="posts">
-        {posts.map((post) => (
-          <div key={post.id} className="post-content">
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="post">
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
